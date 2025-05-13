fd9df2e303d2f8f3640ad5b8dcea594b34ed5fe1
Rondi Andrea <andrea.rondi@student.supsi.ch>
2025-05-13 14:42:40 +0200

    Aggiunta FNH flex

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cf56914b326edb0ccb123ffdc60f00060bd513
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2818434223"
+    serviceWorkerVersion: "2371180209"
   }
 });
